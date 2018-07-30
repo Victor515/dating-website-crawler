3e84e60f3b650a99926850c8676a7c9c91744868
victor515 <victorlevian@gmail.com>
2018-07-30 17:50:42 +0800

    write testcases for citylistParser

--- zhenai/parser/citylist_test.go
+++ zhenai/parser/citylist_test.go
@@ -2,7 +2,6 @@ package parser
 
 import (
 	"testing"
-	"fmt"
 	"io/ioutil"
 )
 
@@ -13,5 +12,38 @@ func TestParseCityList(t *testing.T) {
 		panic(err)
 	}
 
-	fmt.Printf("%s\n", contents)
+	result := ParseCityList(contents)
+
+	const resultSize = 470
+	var expectedUrls = []string{
+		"http://www.zhenai.com/zhenghun/aba",
+		"http://www.zhenai.com/zhenghun/akesu",
+		"http://www.zhenai.com/zhenghun/alashanmeng",
+	}
+
+	var expectedCities = []string{
+		"阿坝",
+		"阿克苏",
+		"阿拉善盟",
+	}
+
+	if len(result.Requests) != resultSize{
+		t.Errorf("Result should have %d results; but have %d", resultSize, len(result.Requests))
+	}
+
+	for i, url := range expectedUrls{
+		if result.Requests[i].Url != url{
+			t.Errorf("expected url #%d: %s; but get %s", i, url, result.Requests[i].Url)
+		}
+	}
+
+	if len(result.Items) != resultSize{
+		t.Errorf("Result should have %d results; but have %d", resultSize, len(result.Items))
+	}
+
+	for i, city := range expectedCities{
+		if result.Items[i].(string) != city{
+			t.Errorf("expected city #%d: %s; but get %s", i, city, result.Items[i])
+		}
+	}
 }
